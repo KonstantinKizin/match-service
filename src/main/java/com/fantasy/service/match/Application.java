a850aed6c179b17b0ae0b86634ae715ac4e38ba0
konstantinkizin <kanstantsin.kizin@gambinoslot.com>
2022-11-14 02:12:18 +0300

    added db properties

--- src/main/java/com/fantasy/service/match/Application.java
+++ src/main/java/com/fantasy/service/match/Application.java
@@ -2,12 +2,13 @@ package com.fantasy.service.match;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"com.fantasy"})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
